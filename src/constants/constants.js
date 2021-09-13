870cea5617192b4e70e04effa398325452cf6997
Andrewmoo <andrewmoo98@gmail.com>
2021-09-13 15:21:52 +0100

    Added Countries app to projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,14 +1,5 @@
 export const projects = [
   {
-    title: 'Lee Campbell Perrie Portfolio',
-    description: "This is a portfolio website I made for an artist friend of mine. it is a fully responsive professional portfolio built using - React, React Hooks, Styled Components, Swiper.js, React Transition Group, React Icons, React Router DOM and more..",
-      image: '/images/LeePortfolio.png',
-      tags: ['React', 'Swiper.js', 'React Router DOM', 'Node'],
-    source: 'https://leecampbellperrie.netlify.app/',
-    visit: 'https://github.com/Andrewmoo/Lees-React-Portfolio',
-    id: 0,
-  },
-  {
     title: 'Delaunay Triangulation',
     description:"Phone case patterns designed and coded in P5.JS using a mathematical function called Delaunay triangulation which invloves ensuring no triangle lays inside the circumcircle of any other triangle to achive this computationally I employed the Bowyer Watson algorithm. I really liked the end product and thought the pattern would fit well on phone cases so from there I produced and marketed the cases",
     image: '/images/2.png',
@@ -16,6 +7,24 @@ export const projects = [
     source: 'https://andrewmoo.github.io/Creative-Coding/Shape-Pattern/Main%20Exercises/Wrapping%20Paper%20Design/index.html',
     visit: 'https://codepen.io/andrewmoo98/pen/YzpBarx?editors=0010',
     id: 1,
+  },
+  {
+    title: 'Countries API App',
+    description: "This app was developed as a coding task for a job interview. the task was to develop a web app centred around data from the Rest Countries API, the spec sheet called for a minimum of 2 pages 1) [A list of all countries where each country should show at a minimum the following information: Name, Flag, Population] and 2) [A detailed view of each country that displays the following data: flag, name, capital, currency, population, languages for the country.] Additional requirements included usage of the Axios/Fetch API, full responsivness, usage of React and avoiding of design frameworks such as material UI or Bootstrap",
+      image: '/images/countriesapp.png',
+      tags: ['React', 'Axios', 'React Router DOM', 'Node'],
+    source: 'https://csb-2erq4.netlify.app/',
+    visit: 'https://codesandbox.io/s/countries-app-2erq4?file=/src/components/Header.js',
+    id: 4,
+  },
+  {
+    title: 'Lee Campbell Perrie Portfolio',
+    description: "This is a portfolio website I made for an artist friend of mine. it is a fully responsive professional portfolio built using - React, React Hooks, Styled Components, Swiper.js, React Transition Group, React Icons, React Router DOM and more..",
+      image: '/images/LeePortfolio.png',
+      tags: ['React', 'Swiper.js', 'React Router DOM', 'Node'],
+    source: 'https://leecampbellperrie.netlify.app/',
+    visit: 'https://github.com/Andrewmoo/Lees-React-Portfolio',
+    id: 0,
   },
   {
     title: 'Stopover',
@@ -42,7 +51,7 @@ export const projects = [
     tags: ['Python', 'SciKit-Learn', 'NumPy'],
     source: 'https://andrew-bell-portfolio.web.app/Andrew%20Bell-N00160591-Thesis.pdf',
     visit: 'https://andrew-bell-portfolio.web.app/Better%20recommender.html',
-    id: 3,
+    id: 5,
   },
 ];
 
