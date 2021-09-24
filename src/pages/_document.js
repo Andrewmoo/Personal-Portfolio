92fd7a2740ec0edf8a7378772210c33d396d7146
Andrewmoo <andrewmoo98@gmail.com>
2021-09-24 13:06:50 +0200

    Added title to document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,6 +34,7 @@ export default class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
           <link rel="icon" href="vercel.svg" />
+          <title>Andrew Bell</title>
           </Head>
         <body>
           <Main />
